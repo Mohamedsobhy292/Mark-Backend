a8940ddf7db1ef22bba3120926d8701b74d9ff20
Mohamed Sobhy <mohamedsobhy292@gmail.com>
2017-10-05 16:09:43 +0200

    config example

---
+++ config-example.js
@@ -0,0 +1,30 @@
+const config = {
+  development: {
+    database: {
+      url: 'mongodb//',
+    },
+    server: {
+      host: '127.0.0.1',
+      port: '3422',
+    },
+  },
+  production: {
+    // url to be used in link generation
+    url: 'http://my.site.com',
+    // mongodb connection settings
+    database: {
+      host: '127.0.0.1',
+      port: '27017',
+      db: 'site',
+    },
+    // server details
+    server: {
+      host: '127.0.0.1',
+      port: '3421',
+    },
+  },
+  jwt: {
+    secret: 'secret',
+  },
+};
+module.exports = config;
