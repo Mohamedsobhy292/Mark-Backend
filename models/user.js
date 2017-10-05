9c7b4cb8a5058b35dd893379181e7fc80a77b153
Mohamed Sobhy <mohamedsobhy292@gmail.com>
2017-10-05 16:11:37 +0200

    user schema

---
+++ models/user.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+mongoose.Promise = global.Promise;
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: 'Please check your mail',
+    trim: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: 'you must specify password',
+  },
+});
+
+userSchema.methods.verifyPassword = function verifyPassword(password) {
+  return bcrypt.compare(password, this.password);
+}
+
+module.exports = mongoose.model('User', userSchema);
